e549198389bf2940c7234f79bfd37c7a12325b0b
PankornSupercool_PC <47814115+sepankorn@users.noreply.github.com>
2021-04-05 18:03:10 +0700

    Database complete (ORM)

---
+++ ASP.NET Implementation/ASP.NET Implementation/Data/Migrations/20210405100034_initailsetup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP.NET_Implementation.Data.Migrations
+{
+    public partial class initailsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Joke",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Question = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Answer = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Joke", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Joke");
+        }
+    }
+}
